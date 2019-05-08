1c5d4656fda8d0d52c78dd87c5e7ff27b67abc06
Daniel <jmzwcn@gmail.com>
2019-05-08 23:29:39 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/home/session/view/view.page.ts
+++ src/app/home/session/view/view.page.ts
@@ -1,5 +1,6 @@
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Book, Page } from '../../../../sdk/book_pb';
 import { apiService, utilService } from '../../../service/api.service';
 
@@ -9,12 +10,12 @@ import { apiService, utilService } from '../../../service/api.service';
   styleUrls: ['./view.page.scss'],
 })
 export class ViewPage implements OnInit {
+  @ViewChild('slider') slides: IonSlides;
   host = utilService.host;
   book: Book.AsObject;
   slideOpts = {
     slidesPerView: 1,
     effect: 'flip',
-    preloadImages: false,
     autoplay: {
       delay: 2000,
       stopOnLastSlide: true,
@@ -28,11 +29,21 @@ export class ViewPage implements OnInit {
     this.book = utilService.book;
   }
 
+  ionViewDidEnter() {
+    this.playSound(this.book.pageList[0]);
+  }
+
+  slideChange(event) {
+    this.slides.getActiveIndex().then(e => {
+      this.playSound(this.book.pageList[e]);
+    });
+  }
+
   playSound(page: Page.AsObject) {
     new Audio(utilService.host + '/uploads/' + page.sound.url).play();
   }
 
   back() {
-    this.router.navigateByUrl('session');
+    this.router.navigateByUrl('home');
   }
 }
