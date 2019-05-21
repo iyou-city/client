25cd1db6a3db7c9fec9e643a095a4f69de1cf98a
Daniel <jmzwcn@gmail.com>
2019-05-21 23:21:47 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/home/contact/contact.page.ts
+++ src/app/home/contact/contact.page.ts
@@ -1,7 +1,9 @@
 import { User } from '../../../sdk/user_pb';
+import { Topic } from '../../../sdk/message_pb';
 import { Group } from '../../../sdk/group_pb';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
 import { apiService, utilService } from '../../service/api.service';
 
 @Component({
@@ -12,12 +14,15 @@ import { apiService, utilService } from '../../service/api.service';
 export class ContactPage implements OnInit {
   users: User.AsObject[] = [];
   groups: Group.AsObject[] = [];
+  msgCache = utilService.msgCache;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
     this.loadGroups();
     this.loadUsers();
+    this.peersMessages();
+    this.groupsMessages();
   }
 
   loadGroups() {
@@ -52,4 +57,55 @@ export class ContactPage implements OnInit {
   gotoGroup() {
     this.router.navigateByUrl('group');
   }
+
+  peersMessages() {
+    let tsUser = new User();
+    tsUser.setId(utilService.getUser().id);
+    let stream = apiService.messageClient.receive(tsUser, apiService.metaData);
+    stream.on('data', response => {
+      let msg = response.toObject();
+      console.log(msg);
+      if (this.msgCache.get(msg.from) == null) {
+        this.msgCache.set(msg.from, []);
+      }
+      this.msgCache.get(msg.from).push(msg)
+    });
+    stream.on('error', err => {
+      console.log(err);
+      this.peersMessages();
+    });
+
+
+  }
+
+  groupsMessages() {
+    let stream = apiService.groupClient.list(new Empty(), apiService.metaData);
+    stream.on('data', response => {
+      let group = response.toObject();
+      this.subscribeTopic(group);
+    });
+    stream.on('error', err => {
+      console.log(err);
+      this.groupsMessages();
+    });
+  }
+
+  subscribeTopic(group: Group.AsObject) {
+    let tsTopIc = new Topic();
+    tsTopIc.setGroupid(group.id);
+    let stream = apiService.messageClient.subscribe(tsTopIc, apiService.metaData);
+    stream.on('data', response => {
+      let topic = response.toObject();
+      console.log(topic);
+      if (this.msgCache.get(topic.groupid) == null) {
+        this.msgCache.set(topic.groupid, []);
+      }
+      this.msgCache.get(topic.groupid).push(topic.message)
+    });
+    stream.on('error', err => {
+      console.log(err);
+      this.subscribeTopic(group);
+    });
+  }
+
 }
