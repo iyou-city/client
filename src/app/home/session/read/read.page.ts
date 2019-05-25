aa350430c20ea34aab7273e6f38a675a1786cc58
Daniel <jmzwcn@gmail.com>
2019-05-25 15:08:31 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/home/session/read/read.page.ts
+++ src/app/home/session/read/read.page.ts
@@ -70,7 +70,8 @@ export class ReadPage implements OnInit {
   }
 
   playOringial(page: Page.AsObject) {
-    new Audio(environment.webUrl + '/uploads/' + page.sound.url).play();
+    // new Audio(environment.webUrl + '/uploads/' + page.sound.url).play();
+    new Audio('https://sp0.baidu.com/-rM1hT4a2gU2pMbgoY3K/gettts?lan=en&text=' + page.name + '&spd=2&source=alading').play();
   }
 
   async playYours(page: Page.AsObject, book: Book.AsObject) {
