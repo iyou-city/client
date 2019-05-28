9240c93d6737f30092ac8630bf23e88004d83676
Daniel <jmzwcn@gmail.com>
2019-05-28 22:26:32 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/home/session/read/read.page.ts
+++ src/app/home/session/read/read.page.ts
@@ -75,7 +75,7 @@ export class ReadPage implements OnInit {
     new Audio('https://sp0.baidu.com/-rM1hT4a2gU2pMbgoY3K/gettts?lan=en&text=' + page.name + '&spd=2&source=alading').play();
   }
 
-  manualPlay(page: Page.AsObject) {
+  onClick(page: Page.AsObject) {
     this.playOringial(page);
     this.play(false);
   }
