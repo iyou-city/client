5cbe9dc0c7c2714065478375b1a219eb20dda948
Daniel <jmzwcn@gmail.com>
2019-06-03 22:23:30 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/home/session/read/read.page.ts
+++ src/app/home/session/read/read.page.ts
@@ -40,8 +40,10 @@ export class ReadPage implements OnInit {
   ngOnInit() {
     this.book = utilService.book;
     // workaround: grant privilege
-    this.onPress((new Page()).toObject(), this.book);
-    this.onPressUp((new Page()).toObject(), this.book);
+    let testPage = new Page();
+    testPage.setName('test');
+    this.onPress(testPage.toObject(), this.book);
+    this.onPressUp(testPage.toObject(), this.book);
   }
 
   ionViewDidEnter() {
