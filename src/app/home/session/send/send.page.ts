257e6967e1dc7ccfe9871267eefa47811d606eb9
Daniel <jmzwcn@gmail.com>
2019-06-16 10:45:13 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/home/session/send/send.page.ts
+++ src/app/home/session/send/send.page.ts
@@ -1,6 +1,6 @@
 import * as grpcWeb from 'grpc-web';
 import { Events } from '@ionic/angular';
-import { Message } from '../../../../sdk/message_pb';
+import { Message, Topic } from '../../../../sdk/message_pb';
 import { Component, OnInit } from '@angular/core';
 import { apiService, utilService } from '../../../service/api.service';
 import { Timestamp } from 'google-protobuf/google/protobuf/timestamp_pb';
@@ -35,11 +35,27 @@ export class SendPage implements OnInit {
     let tt = new Timestamp();
     tt.fromDate(new Date())
     tsMessage.setCreated(tt);
-    apiService.messageClient.send(tsMessage, apiService.metaData, (err: grpcWeb.Error, e: any) => {
-      if (err) {
-        utilService.alert(err.code + ':' + err.message);
-      }
-    });
+    // e.g.139/153/159***
+    if (this.peerUserId.startsWith('1')) {
+      apiService.messageClient.send(tsMessage, apiService.metaData, (err: grpcWeb.Error, e: any) => {
+        if (err) {
+          utilService.alert(err.code + ':' + err.message);
+        }
+      });
+    }
+    
+    // groupId start with 2
+    if (this.peerUserId.startsWith('2')) {
+      let topic = new Topic();
+      topic.setGroupid(this.peerUserId);
+      topic.setMessage(tsMessage);
+      apiService.messageClient.publish(topic, apiService.metaData, (err: grpcWeb.Error, e: any) => {
+        if (err) {
+          utilService.alert(err.code + ':' + err.message);
+        }
+      });
+    }
+
     this.messages.push(tsMessage.toObject());
     this.message.content = '';
   }
